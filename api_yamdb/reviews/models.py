c784163bb7cc16e2a26fdcce312d1db3a9e1e4a3
VANGAZOR <asei3k@gmail.com>
2022-10-04 10:03:13 +0300

    доделал модель, остался валидатор

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,18 +1,38 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+ADMIN = 'admin'
+MODERATOR = 'moderator'
+USER = 'user'
 ROLES_CHOICES = [
-    ('ADMIN', 'admin'),
-    ('MODERATOR', 'moderator'),
-    ('USER', 'user')
+    (ADMIN, ADMIN),
+    (MODERATOR, MODERATOR),
+    (USER, USER)
 
 ]
 
 
 class User(AbstractUser):
+    username = models.CharField(max_length=50,
+                                unique=True,
+                                blank=False,
+                                null=False)
+    # тут не забыть сделать валидатор
+    email = models.EmailField(unique=True,
+                              blank=False,
+                              null=False)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
     bio = models.TextField(
         'Биография',
         blank=True,
     )
-    role = models.CharField(max_length=9, choices=ROLES_CHOICES, default='user')
+    role = models.CharField(max_length=9, choices=ROLES_CHOICES, default=USER)
 
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username

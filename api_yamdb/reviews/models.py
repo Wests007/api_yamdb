cdd5fc2d4ce28ff34f7cc920a7fb0cf817e6905a
Kirill <gridnevka96@gmail.com>
2022-10-04 19:01:00 +0300

    Создал модели - Category, Title, Genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -36,3 +36,67 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Genre(models.Model):
+    name = models.TextField('Название',
+                            blank=False,
+                            max_length=150)
+    slug = models.SlugField('Slug',
+                            blank=False,
+                            unique=True,
+                            db_index=True)
+
+    def __str__(self):
+        return self.name[0:10]
+
+
+class Category(models.Model):
+    name = models.TextField(
+        'Название',
+        blank=False,
+        max_length=150
+    )
+    slug = models.SlugField(
+        'slug',
+        blank=False,
+        unique=True,
+        db_index=True
+    )
+
+    def __str__(self):
+        return self.name[0:10]
+
+
+class Title(models.Model):
+    name = models.TextField(
+        'Название',
+        blank=False,
+        max_length=200,
+        db_index=True
+    )
+    year = models.IntegerField('год', blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+        verbose_name='Категория'
+    )
+    genre = models.ManyToManyField(
+        Genre,
+        blank=True,
+        db_index=True,
+        related_name='titles',
+        verbose_name='Жанр'
+    )
+    description = models.CharField(
+        'Описание',
+        max_length=200,
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name[0:10]

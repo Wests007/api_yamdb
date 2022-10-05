0158670f70d5a68cb5197410ef996a519e052013
Александр Ромашков <eho231@mail.ru>
2022-10-05 19:48:15 +0500

    Создал эндпойнты в урлах

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,15 +1,24 @@
 from rest_framework import routers
 from django.urls import include, path
 
-from .views import (UserViewSet, APISignup, APICode, APIToken)
+from .views import (UserViewSet, ReviewViewSet, CommentViewSet, APISignup, APICode, APIToken)
 
 v1_router = routers.DefaultRouter()
 v1_router.register(r'users', UserViewSet, basename='users')
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+v1_router.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
     path('v1/auth/token/', APIToken),
     path('v1/auth/code/', APICode),
     path('v1/auth/signup/', APISignup),
     path('v1/', include(v1_router.urls)),
-
 ]

ac456c640c1c141343db9cb1090a133769762bcf
Александр Ромашков <eho231@mail.ru>
2022-10-11 09:55:30 +0500

    revert

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -66,7 +66,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
         title_id = self.kwargs.get("title_id")
         serializer.save(
             author=self.request.user,
-            title=get_object_or_404(Title, id=title_id)
+            title=get_object_or_404(Title, id=title_id) 
         )
 
 
